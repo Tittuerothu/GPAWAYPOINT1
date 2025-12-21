c299590a82a7cd9059f9688b4f2109c44fb27555
Tittuerothu <tittu200114@gmail.com>
2025-12-21 13:50:17 +0530

    change4

--- app/src/main/java/com/example/gpswaypoint1/MainActivity.kt
+++ app/src/main/java/com/example/gpswaypoint1/MainActivity.kt
@@ -29,7 +29,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Request location permission
+        // Requesting location permission
+        // Requesting location permission
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
